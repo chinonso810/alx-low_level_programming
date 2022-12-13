e43105efd420d0bbc08dfe0c932be1cb80f4f19a
CHINONSO ORJI <chinonsoe551@gmail.com>
2022-12-12 18:51:30 -0800

    diplaying to standard i/o using puts and printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main -Using the puts function to print to standard output
  * Return: 0 (Success), otherwise 1
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
