6407440e4f46600ac3bef5ebf12655dd96fd6140
CHINONSO ORJI <chinonsoe551@gmail.com>
2022-12-27 23:21:41 -0800

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while(count >=0)
+	while (count >= 0)
 	{
-		if(s[count] == '\0')
+		if (s[count] == '\0')
 			break;
 		count++;
 	}
-	for(count--; count >= 0; count--)
+	for (count--; count >= 0; count--)
 		putchar(s[count]);
 	putchar('\n');
 }
